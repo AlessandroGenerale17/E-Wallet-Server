613cacb7ff153ef0a7fb51df24cb9aab8bfc8b50
rlcngz <erolcengiz@gmail.com>
2021-12-13 14:34:11 +0100

    Set up API Key hidden for Stock end point

--- config/constants.js
+++ config/constants.js
@@ -1,5 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
     CRYPTO_API: 'https://api.coincap.io/v2',
-    API_KEY: process.env.GKEY
+    STOCK_API: 'https://www.alphavantage.co',
+    API_KEY: process.env.API_KEY
 };
--- routes/stock.js
+++ routes/stock.js
@@ -2,6 +2,7 @@
 const axios = require('axios').default;
 const { Router } = require('express');
 const { API_KEY } = require('../config/constants');
+const { STOCK_API } = require('../config/constants');
 
 const router = new Router();
 
@@ -10,7 +11,7 @@ router.route('/:stock').get(async (req, res, next) => {
         // name of stock asset
         const stockSymbol = req.params.stock;
         // request URL using alphavantage
-        const requestUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stockSymbol}&interval=5min&apikey=${API_KEY}`;
+        const requestUrl = `${STOCK_API}/query?function=TIME_SERIES_INTRADAY&symbol=${stockSymbol}&interval=5min&apikey=${API_KEY}`;
         const response = await axios.get(requestUrl);
         console.log(response.data);
         res.status(200).send(response.data);
