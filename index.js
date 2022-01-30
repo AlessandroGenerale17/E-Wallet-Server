82cd0ac46452f4b632faeaf5f62839425f072f2e
AlessandroGenerale17 <a.generale@student.vu.nl>
2022-01-30 09:38:27 +0100

    underscore for unused vars

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 if (process.env.DELAY) {
-    app.use((req, res, next) => {
+    app.use((_req, _res, next) => {
         setTimeout(() => next(), parseInt(process.env.DELAY));
     });
 }
